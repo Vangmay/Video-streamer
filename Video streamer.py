7c9204410f07c636cde303afc62e17e6b5665b1b
Vangmay <78148958+Vangmay@users.noreply.github.com>
2021-03-24 14:21:15 +0530

    Update Video streamer.py

--- Video streamer.py
+++ Video streamer.py
@@ -1,21 +1,35 @@
-#this program will take a youtube url from user and will fetch the video and the title from it,then display the video to the user.
 import pafy
 from tkinter import *
 import vlc
+import time
+from stopwatch import Stopwatch
 root = Tk()
+root.geometry('800x500')
 link = ''
+bg = PhotoImage(file="background.PNG")
+#making canvas
+
 #making input box for entering URL
-url_enter = Entry(root,width = 50)
+photo_label= Label(root,image=bg)
+photo_label.place(x=0,y=0,width=800,height=500)
+entry_frame = Frame(root,bg="#cfcfe2")
+entry_frame.pack(pady=180)
+button_frame = Frame(root)
+button_frame.pack(pady=0)
+url_enter = Entry(entry_frame,width = 50,bg = "#8c56b3")
 url_enter.pack()
 #------------------------------------------------------------------
 #function to make button do something in this case:-
 #take URL and print its title along with playing the video
+
 def submit_url():
     link = url_enter.get()
     video = pafy.new(link)
+    global best
     best = video.getbest()
     playurl = best.url
     Instance = vlc.Instance()
+    global player
     player = Instance.media_player_new()
     media = Instance.media_new(playurl)
     media.get_mrl()
@@ -23,10 +37,29 @@ def submit_url():
     player.play()
     mylabel = Label(root,text = video.title)
     mylabel.pack()
+    duration = video.length
+    
+
+def pause():
+    player.pause()
+def end():
+    player.stop()
+def resume():
+    player.play()
+def download():
+    best.download()
 #function end
 #making button for inputting the URL
-button = Button(root, text = "Submit URL",command = submit_url)
+button = Button(entry_frame, text = "Submit URL",command = submit_url)
 button.pack()
+pause_btn = Button(entry_frame,text = "pause",command = pause)
+pause_btn.pack()
+stop_btn = Button(entry_frame,text="stop",command = end)
+stop_btn.pack()
+resume_btn=Button(entry_frame,text="resume",command = resume)
+resume_btn.pack()
+download_btn = Button(entry_frame,text = 'download',command = download)
+download_btn.pack()
 #__button made___
 #------------------------------------------------------------------
 root.mainloop()
